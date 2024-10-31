30e23b1c946286ea3cf568f4202fd830400e2bfc
Vitaliy <gusevvnn@gmail.com>
2024-11-01 03:29:45 +0500

    correct

--- client/src/service/generateImage.js
+++ client/src/service/generateImage.js
@@ -4,7 +4,7 @@ const DATA_TAG = { type: "Generate", id: "LIST" };
 export const generateApi = createApi({
   reducerPath: "generateApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL,
+    baseUrl: "https://perpetual-emotion-production.up.railway.app/",
   }),
   endpoints: (build) => ({
     generateImage: build.mutation({
